55518a4d90f829e92aac014cbe9e9ed01b5e417b
Mansoor <hussainmansoor876@gmail.com>
2020-02-03 23:43:40 +0500

    initialization

---
+++ App.js
@@ -0,0 +1,77 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
+import NetInfo from "@react-native-community/netinfo";
+
+const { width } = Dimensions.get("window");
+
+function MiniOfflineSign() {
+  return (
+    <View style={styles.offlineContainer}>
+      <Text style={styles.offlineText}>No Internet Connection</Text>
+    </View>
+  );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isConnected: true
+    };
+  }
+
+  componentDidMount() {
+    NetInfo.addEventListener(state => {
+      this.handleConnectivityChange(state.isConnected)
+    });
+  }
+
+  componentWillUnmount() {
+    NetInfo.addEventListener(state => {
+      this.handleConnectivityChange(state.isConnected)
+    });
+  }
+
+  handleConnectivityChange = isConnected => {
+    if (isConnected) {
+      this.setState({ isConnected });
+    } else {
+      this.setState({ isConnected });
+    }
+  };
+
+  render() {
+    if (!this.state.isConnected) {
+      return <MiniOfflineSign />;
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <WebView
+          source={{ uri: 'https://www.elavenn.com/' }}
+          userAgent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  offlineContainer: {
+    backgroundColor: "#b52424",
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    width,
+    position: "absolute",
+    top: 30
+  },
+  offlineText: { color: "#fff" }
+});
+
+export default App;
